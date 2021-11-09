e8019d70a4d6a0f045370578f4bdd45b1e94ae42
Walkerant <330722864@qq.com>
2021-11-09 22:05:10 +0800

    core data support for chat messages

--- ios/Snippets/Snippets/Intrinsic/AppDelegate.h
+++ ios/Snippets/Snippets/Intrinsic/AppDelegate.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
 
 @end
 
